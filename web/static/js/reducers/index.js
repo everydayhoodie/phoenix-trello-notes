9c4412da4940bcc772dd0c8fe2d0cce274f96bc0
everydayhoodie <everydayhoodie@gmail.com>
2016-08-20 20:51:13 +0900

    Phoenix Framework project setup

---
+++ web/static/js/reducers/index.js
@@ -0,0 +1,12 @@
+import {
+  combineReducers
+} from 'redux';
+import {
+  routeReducer
+} from 'redux-simple-router';
+import session from './session';
+
+export default combineReducers({
+  routing: routeReducer,
+  session: session,
+});
---
+++ webpack.config.babel.js
@@ -0,0 +1,62 @@
+'use strict';
+
+import path from 'path';
+import webpack from 'webpack';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+
+function join(dest) {
+    return path.resolve(__dirname, dest);
+}
+
+function web(dest) {
+    return join(`web/static/${dest}`);
+}
+
+var config = module.exports = {
+    entry: {
+        application: [
+            web('css/application.sass'),
+            web('js/application.js'),
+        ],
+    },
+
+    output: {
+        path: join('priv/static'),
+        filename: 'js/application.js',
+    },
+
+    resolve: {
+        extesions: ['', '.js', '.sass'],
+        modulesDirectories: ['node_modules'],
+    },
+
+    module: {
+        noParse: /vendor\/phoenix/,
+        loaders: [{
+            test: /\.jsx$/,
+            exclude: /node_modules/,
+            loader: 'babel',
+            query: {
+                cacheDirectory: true,
+                plugins: ['transform-decorators-legacy'],
+                presets: ['react', 'es2015', 'stage-2', 'stage-0'],
+            },
+        }, {
+            test: /\.sass$/,
+            loader: ExtractTextPlugin.extract('style', 'css!sass?indentedSyntax&includePaths[]=' + __dirname + '/node_modules'),
+        }, ],
+    },
+
+    plugins: [
+        new ExtractTextPlugin('css/application.css'),
+    ],
+};
+
+if (process.env.NODE_ENV === 'production') {
+    config.plugins.push(
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            minimize: true
+        })
+    );
+}
